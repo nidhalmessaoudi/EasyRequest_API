8717389074a49880eb64c5bdb303f506c2aae0d0
Nidhal Messaoudi <nidhalmessaoudi@hotmail.com>
2021-08-29 18:17:45 +0100

    Fixing a bug

--- controllers/requestController/index.js
+++ controllers/requestController/index.js
@@ -2,8 +2,9 @@ const Request = require("../../models/Request/index");
 
 exports.postRequest = async function (req, res) {
   try {
-    if (!req.body.isSuccessful)
+    if (req.body.isSuccessful === null || req.body.isSuccessful === undefined) {
       throw new Error("The request body is missing 'isSuccessful' field");
+    }
     const request = await Request.create(req.body);
     res.status(201).json({
       status: "success",
